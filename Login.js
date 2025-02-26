eaf063fd0f3bfc24903951dea9bacaabe4802a5d
Sabarishwaran-B <sabarish260904@gmail.com>
2025-02-26 10:35:59 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,6 +1,7 @@
 import { FaUserCircle } from "react-icons/fa";
 import { BiLogInCircle } from "react-icons/bi";
 import { FaLock } from "react-icons/fa6";
+import "./Login.css"
 const Login = () => {
     
   return (
